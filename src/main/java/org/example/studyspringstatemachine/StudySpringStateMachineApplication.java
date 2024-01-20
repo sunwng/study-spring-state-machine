f7005b4769838e8fe10e6be45da3373ee94fc7f8
sunwng <sunwgna@gmail.com>
2024-01-20 13:11:31 +0900

    init

---
+++ src/main/java/org/example/studyspringstatemachine/StudySpringStateMachineApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyspringstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/studyspringstatemachine/StudySpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studyspringstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
